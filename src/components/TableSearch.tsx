50f4e6af112ca1d6e9f812b733331c74488876d8
oran <oranefroni9@gmail.com>
2025-01-01 10:45:07 +0200

    fix tablesearch component

--- src/components/TableSearch.tsx
+++ src/components/TableSearch.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 
 type TableSearchProps = {
-  value: string;
-  onChange: (value: string) => void;
+  value?: string;
+  onChange?: (value: string) => void;
 };
 
-const TableSearch = ({ value, onChange }: TableSearchProps) => {
+const TableSearch = ({ value="", onChange }: TableSearchProps) => {
   return (
     <div className="w-full md:w-auto flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
       <Image src="/search.png" alt="" width={14} height={14} />
@@ -13,7 +13,7 @@ const TableSearch = ({ value, onChange }: TableSearchProps) => {
         type="text"
         placeholder="Search..."
         value={value}
-        onChange={(e) => onChange(e.target.value)}
+        onChange={(e) => onChange?.(e.target.value)}
         className="w-[200px] p-2 bg-transparent outline-none"
       />
     </div>
